b8adaf8e5c038bea2cc7fc9b57ff40b3465164af
amandinepg <a.pauloguieu@gmail.com>
2018-07-16 16:11:01 +0200

    updated

---
+++ nodeLookUpEditNoTab.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TK.NodalEditor
+{
+    public partial class nodeLookUpEditNoTab : LookUpEdit
+    {
+        public nodeLookUpEditNoTab()
+        {
+            InitializeComponent();
+        }
+
+        public nodeLookUpEditNoTab(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override bool IsInputKey(Keys KeyData)
+        {
+            if (KeyData == Keys.Tab)
+            {
+                return true;
+            }
+
+            return base.IsInputKey(KeyData);
+        }
+    }
+}
