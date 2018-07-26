0b5dd03c0a7e530edd68112423f4dcf2d2e8eb37
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2018-07-26 19:05:10 +0200

    Added files for python interpreter and DevExpress XtraRichEditor

---
+++ api/Sharpy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+
+namespace TK.NodalEditor
+{
+    public class Sharpy : DynamicObject
+    {
+        public Sharpy()
+        {
+
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = null;
+            switch (binder.Name)
+            {
+                case "addNode":
+                    result = (Func<string, string, int, int, string>)((string a, string b, int c, int d) => NodalDirector.AddNode(a, b, c, d));
+                    return true;
+                case "deleteNode":
+                    result = (Func<string, bool>)((string a) => NodalDirector.DeleteNode(a));
+                    return true;
+            }
+            return false;
+        }
+    }
+}
